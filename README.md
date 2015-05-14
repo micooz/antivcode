f8b7c827820fd75babe1a58495a04c3b335f5143
Micooz <Micooz@hotmail.com>
2015-05-14 22:35:52 +0800

    Use C++11 standard.

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-#Antivcode
+# Antivcode
 
 An identification code validation procedures implement by C++ with boost and libjpeg.
 
 Note: For *personal use* at present.
 
-##Dependence
+## Dependence
+
 * boost v1_56_0 http://www.boost.org/users/history/version_1_56_0.html
 * jpeglib v9(**included**) http://www.ijg.org/files/
 * gcc 4.8.2 or above
 * msvc compiler which support c++11
-##Compile
 
-####Linux
+## Compile
+
+#### Linux
 
 Suppose you have compiled boost library in **/usr/local/boost_1_56_0/** as well as the **.a** static library are in
 **/usr/local/boost_1_56_0/stage/lib**.
@@ -23,43 +25,44 @@ When all of the work is finished,just do following steps:
 ```
 >> cd ./libjpeg/
 >> make
->> cd ../decode/
+>> cd ../antivcode/
 >> make
->> ./decode
+>> ./antivcode
 ```
 
-####Windows
+#### Windows
+
 * Open **.sln** file.
 * Configure include path of boost and libjpeg.
 * Compile libjpeg project first in the solution.
 * Configure attach library folder of boost and libjpeg.
-* Compile decode project.
+* Compile antivcode project.
 * F5 or Ctrl+F5 to run this application.
 
-#Usage
+# Usage
 
 First you should let the program study and generate attribute code database.
 
 To get help information:
 
 ```
->> decode -h
+>> antivcode -h
 Or
->> decode --help
+>> antivcode --help
 ```
 
 To study by filenames from ./folder and create a new database named "db" in ./database:
 
 ```
->> decode -syn -d ./folder/ -b ./database/db
+>> antivcode -syn -d ./folder/ -b ./database/db
 Or
->> decode -s -y -n -d ./folder/ -b ./database/db
+>> antivcode -s -y -n -d ./folder/ -b ./database/db
 ```
 
 To study sigle file and append to database by default(./db):
 
 ```
->> decode -s -f simples/studied.jpg
+>> antivcode -s -f simples/studied.jpg
 ```
 
 if you want to see binaryzated image, just add -v option.
@@ -69,28 +72,29 @@ When finish studying, you can run a test or recognition.
 To test a single file:
 
 ```
->> decode -t -f simples/tested.jpg
+>> antivcode -t -f simples/tested.jpg
 ```
 
 To test a series of images:
 
 ```
->> decode -t -d test/
+>> antivcode -t -d test/
 ```
 Note: test procedure require that file named with a right name.
 
 To recognite a single file:
 
 ```
->> decode -f simples/recognited.jpg
+>> antivcode -f simples/recognited.jpg
 ```
 
 To recognite a series of files:
 
 ```
->> decode -d simples/
+>> antivcode -d simples/
 ```
-#Author
+
+# Author
 
 Micooz
  
--- src/main.cpp
+++ src/main.cpp
@@ -19,264 +19,275 @@ using namespace boost::program_options;
 
 namespace fs = boost::filesystem;
 
-std::ostream& operator<<(std::ostream &out, const CharSet &charset) {
-    out << std::string(charset.begin(), charset.end());
-    return out;
+std::ostream& operator<<(std::ostream& out, const CharSet& charset) {
+  out << std::string(charset.begin(), charset.end());
+  return out;
 }
 
-void study(const string& file, std::shared_ptr<Study> sd, const variables_map *vm) {
-    char buf[10];
-    memset(buf, 0, sizeof(buf));
+void study(const string& file, std::shared_ptr<Study> sd,
+           const variables_map* vm) {
+  char buf[10];
+  memset(buf, 0, sizeof(buf));
 
-    if (vm->count("byname")) {
-        string fname = fs::basename(fs::path(file));
-        strcpy(buf, fname.c_str());
+  if (vm->count("byname")) {
+    string fname = fs::basename(fs::path(file));
+    strcpy(buf, fname.c_str());
+  } else {
+    cout << "please input the characters you saw: " << endl;
+
+    string fullpath = fs::system_complete(fs::path(file)).string();
+    string command = string("start ").append(fullpath);
+    system(command.c_str());
+
+    std::cin >> buf;
+  }
+  cout << "Studying... " << file << endl;
+
+  std::shared_ptr<IImage> jpeg(new Jpeg(file));
+  jpeg->makeGray();
+  jpeg->binaryZate();
+
+  if (vm->count("savetmp")) {
+    string tmpfile = file.substr(0, file.find_last_of('.')) + ".bmp";
+    jpeg->saveTo(tmpfile);
+  }
+
+  auto psc = jpeg->cut();
+  while (true) {
+    if (psc->size() == std::strlen(buf))
+      break;
+    std::cout << "error characters[\"" << buf << "\"] length(right is " <<
+    psc->size() << "), please try again(q to quit): " << endl;
+    std::cin.clear();
+    std::cin >> buf;
+    if (buf[0] == 'q' && buf[1] == 0) {
+      throw std::logic_error("user abort.");
+    }
+  }
+  for (size_t i = 0; i < psc->size(); i++) {
+    sd->confirm(psc->at(i), buf[i]);
+  }
+}
+
+void recognite(std::shared_ptr<Decoder> decoder, const string& file,
+               const variables_map* vm,
+               const std::function<void(const CharSet&)> callback) {
+  //check the file type
+  string extension = fs::extension(fs::path(file));
+  if (extension != ".jpg" && extension != ".jpeg") {
+    throw std::logic_error("invalid image format.");
+  }
+  //for test procedure
+  bool testproc = (vm->count("test")) ? true : false;
+  clock_t time_start = 0;
+  if (testproc) {
+    time_start = clock();
+  }
+  //start decoding
+  CharSet charset = decoder->decode(file);
+
+  callback(charset);
+  //for test procedure
+  if (testproc) {
+    if (string(charset.begin(), charset.end()) ==
+        fs::basename(fs::path(file))) {
+      cout << " √ ";
     } else {
-        cout << "please input the characters you saw: " << endl;
-
-        string fullpath = fs::system_complete(fs::path(file)).string();
-        string command = string("start ").append(fullpath);
-        system(command.c_str());
-
-        std::cin >> buf;
+      cout << " × ";
     }
-    cout << "Studying... " << file << endl;
-
-    std::shared_ptr<IImage> jpeg(new Jpeg(file));
-    jpeg->makeGray();
-    jpeg->binaryZate();
-
-    if (vm->count("savetmp")) {
-        string tmpfile = file.substr(0, file.find_last_of('.')) + ".bmp";
-        jpeg->saveTo(tmpfile);
-    }
-
-    auto psc = jpeg->cut();
-    while (true) {
-        if (psc->size() == std::strlen(buf))
-            break;
-        std::cout << "error characters[\"" << buf << "\"] length(right is " << psc->size() << "), please try again(q to quit): " << endl;
-        std::cin.clear();
-        std::cin >> buf;
-        if (buf[0] == 'q' && buf[1] == 0) {
-            throw std::logic_error("user abort.");
-        }
-    }
-    for (size_t i = 0; i < psc->size(); i++) {
-        sd->confirm(psc->at(i), buf[i]);
-    }
-}
-
-void recognite(std::shared_ptr<Decoder> decoder, const string &file,
-               const variables_map *vm, const std::function<void(const CharSet &)> callback) {
-    //check the file type
-    string extension = fs::extension(fs::path(file));
-    if (extension != ".jpg" && extension != ".jpeg") {
-        throw std::logic_error("invalid image format.");
-    }
-    //for test procedure
-    bool testproc = (vm->count("test")) ? true : false;
-    clock_t time_start = 0;
-    if (testproc) {
-        time_start = clock();
-    }
-    //start decoding
-    CharSet charset = decoder->decode(file);
-
-    callback(charset);
-    //for test procedure
-    if (testproc) {
-        if (string(charset.begin(), charset.end()) == fs::basename(fs::path(file))) {
-            cout << " √ ";
-        } else {
-            cout << " × ";
-        }
-        clock_t now = clock();
-        cout << "duration: " << (double)(now - time_start) / (double)CLOCKS_PER_SEC * 1000  << " ms" << endl;
-    }
+    clock_t now = clock();
+    cout << "duration: " <<
+    (double) (now - time_start) / (double) CLOCKS_PER_SEC * 1000 << " ms" <<
+    endl;
+  }
 }
 
 class examples {
-public:
-    static examples* getInstance() {
-        static examples *_obj = nullptr;
-        static gc _gc;
+ public:
+  static examples* getInstance() {
+    static examples* _obj = nullptr;
+    static gc _gc;
 
-        if (!_obj) {
-            _obj = new examples;
-        }
-        return _obj;
+    if (!_obj) {
+      _obj = new examples;
     }
+    return _obj;
+  }
 
-    friend std::ostream& operator<<(std::ostream &out, const examples *obj) {
-        for (auto line : obj->_text) {
-            out << line << std::endl;
-        }
-        return out;
+  friend std::ostream& operator<<(std::ostream& out, const examples* obj) {
+    for (auto line : obj->_text) {
+      out << line << std::endl;
     }
-private:
-    examples() {
-        _text.reserve(30);
-        ifstream in(_file, ios::in | ios::binary);
+    return out;
+  }
 
-        string buf;
-        if (in) {
-            while (!in.eof()) {
-                std::getline(in, buf);
-                _text.push_back(buf);
-            }
-            in.close();
-        }
+ private:
+  examples() {
+    _text.reserve(30);
+    ifstream in(_file, ios::in | ios::binary);
+
+    string buf;
+    if (in) {
+      while (!in.eof()) {
+        std::getline(in, buf);
+        _text.push_back(buf);
+      }
+      in.close();
     }
+  }
 
-    class gc {
-    public:
-        ~gc() {
-            examples *pIns = examples::getInstance();
-            if (pIns) {
-                delete pIns;
-                pIns = nullptr;
-            }
-        }
-    };
-    vector<string> _text;
-    const string _file = "./examples";
+  class gc {
+   public:
+    ~gc() {
+      examples* pIns = examples::getInstance();
+      if (pIns) {
+        delete pIns;
+        pIns = nullptr;
+      }
+    }
+  };
+
+  vector<string> _text;
+  const string _file = "./examples";
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 
 #if defined (WIN32) || (_WIN32)
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-    boost::program_options::options_description opts("decode Usage");
-    opts.add_options()
-        ("help,h", "show help information")
-        ("study,s", "auto create char map database, require -d or -f")
-        ("byname,y", "study by filename without input manually")
-        ("directory,d", value<string>(), "directory where vcode images saved in")
-        ("file,f", value<string>(), "vcode image path")
-        ("database,b", value<string>()->default_value("./db"), "database file path")
-        ("test,t", "run test procedure, require -d or -f")
-        ("newdb,n", "create new database")
-        ("savetmp,v", "save binaryzated file, require -s");
+  boost::program_options::options_description opts("decode Usage");
+  opts.add_options()
+          ("help,h", "show help information")
+          ("study,s", "auto create char map database, require -d or -f")
+          ("byname,y", "study by filename without input manually")
+          ("directory,d", value<string>(),
+           "directory where vcode images saved in")
+          ("file,f", value<string>(), "vcode image path")
+          ("database,b", value<string>()->default_value("./db"),
+           "database file path")
+          ("test,t", "run test procedure, require -d or -f")
+          ("newdb,n", "create new database")
+          ("savetmp,v", "save binaryzated file, require -s");
 
-    variables_map vm;
-    try {
-        store(parse_command_line(argc, argv, opts), vm);
-    } catch (exception &ex) {
-        cout << ex.what() << endl;
-        return -1;
-    }
-    notify(vm);
+  variables_map vm;
+  try {
+    store(parse_command_line(argc, argv, opts), vm);
+  } catch (exception& ex) {
+    cout << ex.what() << endl;
+    return -1;
+  }
+  notify(vm);
 
-    try {
-        //circulate just once, only one exit at return 0
-        do {
-            //show help
-            if (vm.count("help")) {
-                cout << opts << examples::getInstance() << endl;
-                break;
+  try {
+    //circulate just once, only one exit at return 0
+    do {
+      //show help
+      if (vm.count("help")) {
+        cout << opts << examples::getInstance() << endl;
+        break;
+      }
+
+      //study process
+      if (vm.count("study")) {
+        if (!(vm.count("directory") || vm.count("file"))) {
+          throw std::logic_error("-d or -f must be set.");
+        }
+
+        fs::path dbpath(vm["database"].as<string>());
+
+        if (vm.count("newdb")) {
+          if (fs::exists(dbpath)) {
+            fs::remove(dbpath);
+          }
+        }
+
+        std::shared_ptr<Study> sd(new Study(dbpath.string()));
+
+        if (vm.count("directory")) {
+          fs::path dir(vm["directory"].as<string>());
+          if (!fs::exists(dir)) {
+            fs::create_directory(dir);
+          }
+          fs::recursive_directory_iterator beg(dir);
+          fs::recursive_directory_iterator end;
+          for (; beg != end; beg++) {
+            string extension = fs::extension(fs::path(*beg));
+            if (extension == ".jpg" || extension == ".jpeg") {
+              study(beg->path().string(), sd, &vm);
             }
+          }
+        } else if (vm.count("file")) {
+          study(vm["file"].as<string>(), sd, &vm);
+        }
 
-            //study process
-            if (vm.count("study")) {
-                if (!(vm.count("directory") || vm.count("file"))) {
-                    throw std::logic_error("-d or -f must be set.");
+        sd->finish();
+
+        cout << "study finished, database saved at " <<
+        vm["database"].as<string>() << endl;
+
+        break;
+      }
+
+      //recognition process
+      if (vm.count("file") || vm.count("directory")) {
+        if (!fs::exists(fs::path(vm["database"].as<string>()))) {
+          throw std::logic_error("database not found.");
+        }
+        string db = vm["database"].as<string>();
+        std::shared_ptr<Decoder> decoder(new Decoder(db));
+
+        if (vm.count("file")) {
+          //for single file
+          string file = vm["file"].as<string>();
+          recognite(decoder, file, &vm, [&](const CharSet& charset) {
+            cout << charset;
+          });
+          break;
+        } else if (vm.count("directory")) {
+          //for batch recognition
+          string dir = vm["directory"].as<string>();
+          if (!fs::exists(fs::path(dir))) {
+            throw std::logic_error("directory not found.");
+          }
+          //traversal the folder
+          int right_count = 0, sumfile = 0;
+          fs::recursive_directory_iterator beg(dir);
+          fs::recursive_directory_iterator end;
+          for (; beg != end; beg++) {
+            string extension = fs::extension(fs::path(*beg));
+            if (extension == ".jpg" || extension == ".jpeg") {
+
+              string file = beg->path().string();
+
+              recognite(decoder, file, &vm, [&](const CharSet& charset) {
+                cout << file << " ----> " << charset << endl;
+                if (std::string(charset.begin(), charset.end())
+                    == fs::basename(fs::path(file))) {
+                  right_count++;
                 }
+                //cout << "\taverage: " << (now - time_start) / sumfile << " ms" << endl;
+              });
 
-                fs::path dbpath(vm["database"].as<string>());
-
-                if (vm.count("newdb")) {
-                    if (fs::exists(dbpath)) {
-                        fs::remove(dbpath);
-                    }
-                }
-
-                std::shared_ptr<Study> sd(new Study(dbpath.string()));
-
-                if (vm.count("directory")) {
-                    fs::path dir(vm["directory"].as<string>());
-                    if (!fs::exists(dir)) {
-                        fs::create_directory(dir);
-                    }
-                    fs::recursive_directory_iterator beg(dir);
-                    fs::recursive_directory_iterator end;
-                    for (; beg != end; beg++) {
-                        string extension = fs::extension(fs::path(*beg));
-                        if (extension == ".jpg" || extension == ".jpeg") {
-                            study(beg->path().string(), sd, &vm);
-                        }
-                    }
-                } else if (vm.count("file")) {
-                    study(vm["file"].as<string>(), sd, &vm);
-                }
-
-                sd->finish();
-
-                cout << "study finished, database saved at " << vm["database"].as<string>() << endl;
-
-                break;
+              sumfile++;
             }
+          }
+          cout << "\nright/sum: " << right_count << "/"
+          << sumfile << " = " << (double) right_count / sumfile << endl;
 
-            //recognition process
-            if (vm.count("file") || vm.count("directory")) {
-                if (!fs::exists(fs::path(vm["database"].as<string>()))) {
-                    throw std::logic_error("database not found.");
-                }
-                string db = vm["database"].as<string>();
-                std::shared_ptr<Decoder> decoder(new Decoder(db));
+          break;
+        }//if
+      }
 
-                if (vm.count("file")) {
-                    //for single file
-                    string file = vm["file"].as<string>();
-                    recognite(decoder, file, &vm, [&] (const CharSet &charset) {
-                        cout << charset;
-                    });
-                    break;
-                } else if (vm.count("directory")) {
-                    //for batch recognition
-                    string dir = vm["directory"].as<string>();
-                    if (!fs::exists(fs::path(dir))) {
-                        throw std::logic_error("directory not found.");
-                    }
-                    //traversal the folder
-                    int right_count = 0, sumfile = 0;
-                    fs::recursive_directory_iterator beg(dir);
-                    fs::recursive_directory_iterator end;
-                    for (; beg != end; beg++) {
-                        string extension = fs::extension(fs::path(*beg));
-                        if (extension == ".jpg" || extension == ".jpeg") {
+      cout << opts << examples::getInstance() << endl;
 
-                            string file = beg->path().string();
+    } while (false);
 
-                            recognite(decoder, file, &vm, [&] (const CharSet &charset) {
-                                cout << file << " ----> " << charset << endl;
-                                if (std::string(charset.begin(), charset.end())
-                                    == fs::basename(fs::path(file))) {
-                                    right_count++;
-                                }
-                                //cout << "\taverage: " << (now - time_start) / sumfile << " ms" << endl;
-                            });
+  } catch (std::logic_error& e) {
+    cout << e.what() << endl;
+  }
 
-                            sumfile++;
-                        }
-                    }
-                    cout << "\nright/sum: " << right_count << "/"
-                        << sumfile << " = " << (double) right_count / sumfile << endl;
-
-                    break;
-                }//if
-            }
-
-            cout << opts << examples::getInstance() << endl;
-
-        } while (false);
-
-    } catch (std::logic_error& e) {
-        cout << e.what() << endl;
-    }
-
-    return 0;
+  return 0;
 }
 
